276cc0c7c87fb3979c6c60c41a716d19c76a346d
Jasper van Noordenburg <jasper.van.noordenburg@gemboxx.nl>
2017-05-17 11:56:07 +0200

    Test push

---
+++ app/src/main/java/uu/mbi/mmob/alternative_buttons/CameraActivity.java
@@ -0,0 +1,17 @@
+package uu.mbi.mmob.alternative_buttons;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jasper on 17-May-17.
+ */
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/uu/mbi/mmob/alternative_buttons/MainActivity.java
+++ app/src/main/java/uu/mbi/mmob/alternative_buttons/MainActivity.java
@@ -1,13 +1,34 @@
 package uu.mbi.mmob.alternative_buttons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton mCameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraButton = (ImageButton) findViewById(R.id.ib_camera);
+        mCameraButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.ib_camera:
+                Intent intent = new Intent(this, CameraActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+
 }
